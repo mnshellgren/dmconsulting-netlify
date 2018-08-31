5e29146450ce6919624ff4b8aba8b63523ddb98f
mnshellgren <id14msn@cs.umu.se>
2018-08-31 15:34:50 +0200

    add icons to cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,21 @@ import graphql from 'graphql'
 import Profilepicture from '../asa_main_profile.jpg'
 import Logotype from './logotype_black.svg'
 
+import leadership_icon from './utveckling.svg'
+import chef_icon from './chef.svg'
+import moderator_icon from './moderator.svg'
+import organisation_icon from './organisation.svg'
+
 const IndexPage = ({ data }) => {
   return (
     <div className="page-content">
       <div className="jumbotron">
-        <h1>En digital framtid kräver ett uppgraderat ledarskap</h1>
+        <h1>Digitalt Ledarskap & Engagerande kommunikation</h1>
       </div>
       <Container>
-        <section className="col-xs-10 col-sm-10 col-md-8 text-center m-auto section-padding-top-lg section-padding-bottom-lg">
+        <section className="col-xs-11 col-sm-10 col-md-8 text-center m-auto section-padding-top-lg section-padding-bottom-lg">
 
-            <h5 className="">Degermark Consulting inriktar sig främst på kommunikation och ledarskap
+            <h5 className="intro-text">Degermark Consulting inriktar sig främst på kommunikation och ledarskap
           och ger stöd till chefer och ledningsgrupper som behöver utveckla sitt
           eget eller ledningsgruppens gemensamma ledarskap i takt med att företaget
           utvecklas och digitaliseras.</h5>
@@ -27,23 +32,31 @@ const IndexPage = ({ data }) => {
         <div className="row">
           <div className="col-xs-12 col-sm-6 col-md-6 mt-5">
             <div className="card p-4 text-center">
+              <img className="card-icon mt-3 mb-4" src={leadership_icon} alt="Created by Massupa  Kaewgahya"/>
               <h4>Ledarskapsutveckling</h4>
+              <p>Bli bättre rustad för att leda framgångsrikt i takt med att ditt företag utvecklas och digitaliseras</p>
             </div>
             <div className="card p-4 text-center mt-4">
+              <img className="card-icon mt-3 mb-4" src={moderator_icon} alt="Created by Massupa  Kaewgahya"/>
               <h4>Moderator & Föredragshållare</h4>
+              <p>Inspirera och engagera din publik inom områdena digitalt ledarskap, kommunikation och förändring</p>
             </div>
           </div>
           <div className="col-xs-12 col-sm-6 col-md-6 mt-5">
             <div className="card p-4 text-center">
+              <img className="card-icon mt-3 mb-4" src={organisation_icon} alt="Created by Massupa  Kaewgahya"/>
               <h4>Kommunikation</h4>
+              <p>Nyckeln till engagemang och förändring börjar med bra kommunikation, både internt och externt </p>
             </div>
             <div className="card p-4 text-center mt-4">
+              <img className="card-icon mt-3 mb-4" src={chef_icon} alt="management by Massupa  Kaewgahya from the Noun Project"/>
               <h4>Interim chef</h4>
+              <p>Stärk upp tillfälligt inom områden som marknad, kommunikation, administration eller liknande</p>
             </div>
 
           </div>
-          <div className="col-12 mt-5">
-            <Link to='/services' className="col-12 main-button inverted">Läs mer</Link>
+          <div className="col-12 mt-5 pt-5 text-center">
+            <Link to='/services' className="col-12 main-button inverted">Läs mer →</Link>
           </div>
 
         </div>
